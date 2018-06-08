c7d106bc7b734af3f7633f8027adf768faa21482
Your Name <you@example.com>
2018-06-08 13:55:13 +0800

    testing1

--- fenge.py
+++ fenge.py
@@ -1,4 +1,4 @@
 import pandas as pd
 chunks=pd.read_csv('./dataProcess/train.csv',header=None,iterator=True)
-chunk=chunks.get_chunk(2000)
+3232323232323232323232323232323232323232323232323232323232323232chunk=chunks.get_chunk(2000)
 chunk.to_csv('./dataProcess/train.csv')
